bbe7ea3aa7d66e9674c390aa020fbc1d91ef3cd8
Yuanda Dong <dongyuanda@gmail.com>
2024-04-07 17:04:21 +1000

    Playlist management

--- src/app/playlists/page.tsx
+++ src/app/playlists/page.tsx
@@ -1,12 +1,27 @@
 "use client";
-import React from "react";
-import { usePlaylists } from "../Context";
+import React, { FormEvent, useState } from "react";
+import { usePlaylists, usePlaylistsDispatch } from "../Context";
 import { PlaylistItem } from "@/components/playlist-item";
 import { useRouter } from "next/navigation";
+import { Button, Form, Modal } from "react-bootstrap";
 
 export default function PlaylistsPage() {
   const playlists = usePlaylists();
   const router = useRouter();
+
+  const dispatch = usePlaylistsDispatch();
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleSubmit = (event: FormEvent<HTMLElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.target as HTMLFormElement);
+    const name = formData.get("name") as string;
+    const description = formData.get("desc") as string;
+    dispatch({ kind: "Addp", name, description });
+    handleClose();
+  };
 
   return (
     <>
@@ -14,11 +29,41 @@ export default function PlaylistsPage() {
         <PlaylistItem
           playlist={p}
           key={p.id}
-          onClick={() => {
+          onClickOuter={() => {
             router.push(`playlists/${p.id}`);
+          }}
+          onClickInner={() => {
+            dispatch({ kind: "removeP", id: p.id });
           }}
         />
       ))}
+
+      <Button variant="primary" onClick={handleShow}>
+        Add playlist
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Create New Playlist</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group>
+              <Form.Label>Playlist name: </Form.Label>
+              <Form.Control placeholder="name" name="name" />
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label>Description: </Form.Label>
+              <Form.Control as="textarea" placeholder="Fun videos" rows={5} name="desc" />
+            </Form.Group>
+
+            <Button variant="primary" type="submit" className="mt-3">
+              Submit
+            </Button>
+          </Form>
+        </Modal.Body>
+      </Modal>
     </>
   );
 }
--- src/components/playlist-item.tsx
+++ src/components/playlist-item.tsx
@@ -1,25 +1,36 @@
-import { Col, Row } from "react-bootstrap";
+import { Button, Col, Row } from "react-bootstrap";
 import { Playlist } from "../interfaces/playlist";
 
 interface PlaylistItemProps {
   playlist: Playlist;
-  onClick: () => void;
+  onClickOuter: () => void;
+  onClickInner: () => void;
 }
 
 export function PlaylistItem(props: PlaylistItemProps) {
-  const { playlist } = props;
+  const { playlist, onClickOuter, onClickInner } = props;
 
   const videoCount =
     playlist.videoIds.length === 1 ? "1 video" : `${playlist.videoIds.length} videos`;
 
   return (
-    <Row className="border rounded p-2 mb-2 hover" onClick={props.onClick}>
+    <Row className="border rounded p-2 mb-2 hover" onClick={onClickOuter}>
       <Col xs="12" md="3">
         <h2 className="h5">{playlist.name}</h2>
         <p className="mb-0">{videoCount}</p>
       </Col>
-      <Col xs="12" md="9">
+      <Col xs="12" md="8">
         <p className="mb-0">{playlist.description}</p>
+      </Col>
+      <Col xs="12" md="1" className="mb-3">
+        <Button
+          onClick={(e) => {
+            e.stopPropagation();
+            onClickInner();
+          }}
+        >
+          Remove playlist
+        </Button>
       </Col>
     </Row>
   );
